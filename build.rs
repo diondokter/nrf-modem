d2825e7822d2e830e3c4b48a0a90cc0cb7a6fadb
Juergen Fitschen <me@jue.yt>
2025-03-24 13:37:36 +0100

    Add modem log output

---
+++ build.rs
@@ -0,0 +1,9 @@
+#[cfg(feature = "modem-log")]
+use cc;
+
+fn main() {
+    #[cfg(feature = "modem-log")]
+    cc::Build::new()
+        .file("wrapper/nrf_modem_os_log.c")
+        .compile("nrf_modem_os_log");
+}
